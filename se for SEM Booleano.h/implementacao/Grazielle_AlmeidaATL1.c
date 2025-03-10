a29eca8a01fbee8a021256cc59a1027e032e8c10
Luana Macedo <92169736+luanagbrm@users.noreply.github.com>
2025-03-10 20:46:44 -0300

    add: implementacao com as bibliotecas e main

---
+++ se for SEM Booleano.h/implementacao/Grazielle_AlmeidaATL1.c
@@ -0,0 +1,85 @@
+/*
+Atividade L1
+arquivo: Grazielle_AlmeidaATL1.c
+Autores: Geovanna Cristina Brenzinger
+		 Grazielle Batista de Almeida
+		 Luana Gabrielle Rodrigues Macedo
+  		 Isabela Nunes dos Santos
+Data: 11/03/2025
+Descricao: verificar existencia de duplicatas em uma lista
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+                        
+void construirNumeros(unsigned char V[]){
+	srand(time(NULL));
+	unsigned char qtd_numeros;
+	unsigned char i;
+	unsigned char j;
+	
+	V[0] = 9;
+ 
+	for(i = 1; i <= 9; i++){
+		V[i] = rand()%9+1;
+	}
+}
+
+void mostrarNumeros(unsigned char V[]){
+	unsigned char i;
+	
+	unsigned char qntNum = V[0];
+	
+	for(i = 1; i <= qntNum; i++){
+		printf("%d ", V[i]);
+	}
+	printf("\n");
+}
+
+bool verificarRepetido(unsigned char V[]){
+	bool tem_duplicata;
+	unsigned char i;
+	unsigned char j;
+	unsigned char qntNum;
+	
+	qntNum = V[0];
+	
+	tem_duplicata = false;
+	i = 1;
+	
+	while(i < qntNum && tem_duplicata != true){
+		j = i + 1;
+		while(j <= qntNum && tem_duplicata != true){
+			if(V[i]==V[j]){
+				tem_duplicata = true;
+			} else {
+				j = j + 1;
+			}	
+		}
+		i = i + 1;
+	}		
+	
+	return tem_duplicata;
+}
+
+int main(){
+	unsigned char V[3] = {2,3,4};
+	mostrarNumeros(V);
+	if(verificarRepetido(V)){
+		printf("tem repetido");
+	} else {
+		printf("nao tem repetido\n");
+	}
+	
+	construirNumeros(V);
+	mostrarNumeros(V);
+	if(verificarRepetido(V)){
+		printf("tem repetido");
+	} else {
+		printf("nao tem repetido");
+	}
+	
+	return 0;
+}
